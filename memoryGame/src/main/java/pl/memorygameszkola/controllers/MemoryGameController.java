7adc2568b1c92cff87e8e779430e4b0a8b82db4b
MikeyGos <m.pielewski@gmail.com>
2024-06-01 19:10:19 +0100

    Add files via upload

---
+++ memoryGame/src/main/java/pl/memorygameszkola/controllers/MemoryGameController.java
@@ -0,0 +1,11 @@
+package pl.memorygameszkola.controllers;
+
+import pl.memorygameszkola.stockOfCard.Card;
+
+public class MemoryGameController extends Card {
+
+
+    public MemoryGameController(String cardName) {
+        super(cardName);
+    }
+}
